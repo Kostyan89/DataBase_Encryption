f11246a6b76e673a7174d840a1a5a9a056581b10
kostyan <kostyan@kostyans-MacBook-Air.local>
2023-08-21 10:36:05 +0300

    implement hashing pass instead encrypting

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
+import md5 from "md5";
 
 const app = express();
-
-// console.log(process.env.API_KEY);
 
 app.set("view engine", "ejs");
 
@@ -19,11 +17,6 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-});
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
 });
 
 const User = new mongoose.model("User", userSchema);
@@ -43,7 +36,7 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const newUser = await new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save();
@@ -52,7 +45,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   const foundUser = await User.findOne({ email: username });
   if (foundUser) {
